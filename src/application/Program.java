9195440a66a830913b86651f87816da3c9ada807
Flavio Nascimento <54750750+FlavioNascimento99@users.noreply.github.com>
2025-01-05 17:10:29 -0300

    Seller entity class
    Added Seller class and update main program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dep = new Department(1, "Livros");
-		System.out.println(dep);
+		
+		Seller sel = new Seller(1, "Jon Jones", "bones@mail.com", new Date(), 8000.0, dep);
+		
+		System.out.println(sel);
 	}
 
 }
